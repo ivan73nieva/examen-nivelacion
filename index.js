090aad50057c348304bba52e78ada4b368bc1e0e
ivan nieva <aleinllano@gmail.com>
2024-03-27 21:29:16 -0300

    examen

---
+++ index.js
@@ -0,0 +1,41 @@
+let tipodefigura = prompt ("ingrese numero de lados");
+
+switch (tipodefigura) {
+    
+    case "0":
+    console.log("es un circulo");
+    break;
+
+    case "3":
+    console.log("es un triangulo");
+    break;
+
+    case "4":
+    console.log("es un cuadrado");
+    break;
+
+    case "5":
+    console.log("es un pentagono");
+    break;
+
+    case "6":
+    console.log("es un hegagono");
+    break;
+
+    case ">6":
+    console.log("es un poligono");
+    break;
+
+    default:
+    console.log("el numero ingresado es invalido");
+
+
+
+
+
+
+
+
+
+}
+
---
+++ solo.js
@@ -0,0 +1,23 @@
+function lados (tipofigura) {
+    console.log("su tipo de figura es" + tipofigura)
+}
+if (tipofigura=0) {
+    console.log("circulo");
+}
+else if (tipofigura=3) {
+    console.log("triangulo");
+}
+else if (tipofigura=4) {
+    console.log("triangulo");
+}
+
+else if (tipofigura=5) {
+    console.log("triangulo");
+}
+else if (tipofigura=6) {
+    console.log("triangulo");
+}
+else (tipofigura>6) {
+    console.log("triangulo");
+}
+
